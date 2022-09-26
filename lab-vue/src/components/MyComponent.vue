443e55efbe95ea172248d53ad5024826daaea271
Judit Bancells Vélez <judit@juditbancells.cat>
2022-09-26 18:27:53 +0200

    iteration 2 & 3

---
+++ lab-vue/src/components/MyComponent.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>hello {{ name }}</div>
+	<button @click="helloWorld()">Button log</button>
+</template>
+
+<script setup>
+	import { ref } from "vue";
+
+	const name = ref("Judit");
+	const helloWorld = () => console.log("Hello World!");
+</script>
+
+<style scoped>
+</style>

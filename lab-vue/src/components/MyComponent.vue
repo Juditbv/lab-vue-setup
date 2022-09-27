06dbaa8c9792d1fb5a5ce8ad8056e9fe933785de
Judit Bancells Vélez <judit@juditbancells.cat>
2022-09-27 09:13:50 +0200

    component correct order

--- lab-vue/src/components/MyComponent.vue
+++ lab-vue/src/components/MyComponent.vue
@@ -1,14 +1,14 @@
-<template>
-	<div>hello {{ name }}</div>
-	<button @click="helloWorld()">Button log</button>
-</template>
-
 <script setup>
 	import { ref } from "vue";
 
 	const name = ref("Judit");
 	const helloWorld = () => console.log("Hello World!");
 </script>
+
+<template>
+	<div>hello {{ name }}</div>
+	<button @click="helloWorld()">Button log</button>
+</template>
 
 <style scoped>
 </style>
